62282e7525c6d732a992748f91c02d7cd7eb3dfe
jihane <outamassoutejihane@gmail.com>
2025-11-19 19:16:16 +0100

    Ajout DossierMedical et Antecedent

--- DentalTech/src/main/java/ma/dentalTech/entities/antecedents/Antecedents.java
+++
@@ -1,4 +0,0 @@
-package ma.dentalTech.entities.antecedents;
-
-public class Antecedents {
-}
--- DentalTech/src/main/java/ma/dentalTech/entities/dossierMedical/DossierMedical.java
+++ DentalTech/src/main/java/ma/dentalTech/entities/dossierMedical/DossierMedical.java
@@ -1,4 +1,38 @@
 package ma.dentalTech.entities.dossierMedical;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import ma.dentalTech.entities.patient.Patient;
+import ma.dentalTech.entities.antecedents.Antecedent;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+@Table(name = "dossiers_medicaux")
 public class DossierMedical {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idDossier;
+
+    @Column(unique = true, nullable = false)
+    private String numeroDossier;
+
+    @Column(name = "date_creation")
+    private LocalDateTime dateCreation;
+
+    @OneToOne
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+
+    @OneToMany(mappedBy = "dossierMedical", cascade = CascadeType.ALL)
+    private List<Antecedent> antecedents;
+
+    @PrePersist
+    public void initDate() {
+        this.dateCreation = LocalDateTime.now();
+    }
 }
