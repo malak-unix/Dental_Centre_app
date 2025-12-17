b7e4f825f7326c007683f19a6fb4ff9dc882bad0
Aicha <aichaelbekali53@gmail.com>
2025-12-17 22:23:55 +0100

    Merge branch 'feature/sprint1-entities-schema' of https://github.com/malak-unix/Dental_Centre_app into feature/sprint1-entities-schema

--- DentalTech/src/main/java/ma/dentalTech/service/modules/patient/api/PatientService.java
+++ DentalTech/src/main/java/ma/dentalTech/service/modules/patient/api/PatientService.java
@@ -7,11 +7,11 @@ import java.util.List;
 public interface PatientService {
     List<Patient> getAll();
     Patient getById(Long id);
+
     void create(Patient p);
     void update(Patient p);
     void delete(Patient p);
     void deleteById(Long id);
-
-    List<Patient> searchByNom(String nomPart);
+    List<Patient> searchByNom(String nom);
     Patient getByTelephone(String telephone);
 }
